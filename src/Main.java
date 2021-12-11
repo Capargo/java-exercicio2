65c226a174693c77c7c55ecb1bf5ae228ebf4098
caroline parra gomes <Cargomes@server01.foursys.local>
2021-12-11 16:54:48 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Crie um programa para que apresente sua idade, altura e peso.
+
+        int idade;
+        double altura;
+        String peso;
+
+        idade = 26;
+        altura = 1.65;
+        peso = "80kg";
+
+        System.out.println(idade);
+        System.out.println(altura);
+        System.out.println(peso);
+    }
+}
